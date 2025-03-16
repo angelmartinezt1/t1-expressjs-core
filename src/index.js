8485876297fb2de7bf57cf86a7323919458430df
angel.martinez <angel.martinez@claroshop.com>
2025-03-16 16:35:48 -0600

    chore(lint): exclude src/index.js from ESLint multiple empty lines rule

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @stylistic/no-multiple-empty-lines */
 export { default as responseMiddleware } from './middleware/responseMiddleware.js'
 export { default as ResponseBuilder } from './utils/responseBuilder.js'
 
