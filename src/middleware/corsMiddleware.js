aa91958d72ade40aed06c91ae367faead2d8a12f
angel.martinez <angel.martinez@claroshop.com>
2025-03-16 21:59:32 -0600

    fix(middleware): add env variables

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -1,10 +1,10 @@
 import cors from 'cors'
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN?.split(',') || '*', // Permitir múltiples orígenes separados por comas
+  origin: process.env.CORS_ORIGIN || '*',
   methods: process.env.CORS_METHODS || 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: process.env.CORS_HEADERS || 'Content-Type,Authorization',
-  credentials: process.env.CORS_CREDENTIALS === 'true', // Convertir string a boolean
+  credentials: Boolean(process.env.CORS_CREDENTIALS === 'true'),
   optionsSuccessStatus: 204
 }
 
